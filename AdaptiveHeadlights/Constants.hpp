bd49082a17d092c8eefe32f58a8d4de1f9290fe1
ikt <6639510+ikt32@users.noreply.github.com>
2023-07-16 10:38:09 +0200

    Update Constants.hpp

--- AdaptiveHeadlights/Constants.hpp
+++ AdaptiveHeadlights/Constants.hpp
@@ -4,7 +4,7 @@
 
 #define VERSION_MAJOR 1
 #define VERSION_MINOR 0
-#define VERSION_PATCH 0
+#define VERSION_PATCH 1
 
 namespace Constants {
     static const char* const ScriptName = "Adaptive Headlights";
